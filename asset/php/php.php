07d04d842e4059cc849ab772e5f62bfa7684de16
ChauvinMathis10 <chauvin.mathis1.0@gmail.com>
2025-12-28 11:49:14 +0100

    Add files via upload

---
+++ asset/php/php.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $nom = htmlspecialchars($_POST["name"]);
+  $email = htmlspecialchars($_POST["email"]);
+  $message = htmlspecialchars($_POST["message"]);
+
+  $to = "chauvin.mathis1.0@gmail.com" 
+  $subject = "Nouveau message de contact";
+  $body = "Nom: $nom\nEmail: $email\nMessage:\n$message";
+  $headers = "From: $email";
+
+  if (mail($to, $subject, $body, $headers)) {
+    echo "Message envoyé avec succès.";
+  } else {
+    echo "Erreur lors de l'envoi du message.";
+  }
+}
+?>
